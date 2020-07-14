9e076418286d491b0d8a756311650c5da37b903c
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-07-14 22:38:16 +0530

    favicon

--- gui/src/index.js
+++ gui/src/index.js
@@ -1,1 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import 'android.chrome-512x512.png'
 import App from "./components/App"
+class Index extends Component {
+    
+    render() {
+        return (
+            <div>
+	            <img src={ 'static/android.chrome-512x512.png' } />
+	            <App />
+            </div>
+        );
+    }
+}
+
+export default Index;
+
+const container = document.getElementById("app");
+ReactDOM.render(<Index />, container);
